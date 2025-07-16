b9ed8a60f22a2a71390d58379913358a06003b11
gabriel <githubgabriel21@gmail.com>
2025-07-15 23:36:06 -0300

    agoravai

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const base_url = process.env.REACT_APP_API_DEVELOPMENT || (
   process.env.NODE_ENV === "development" 
     ? "http://localhost:5000/api" // Local backend URL
-    : "https://sisa-project.up.railway.app/api" // Correct backend URL without port
+    : "sisa.up.railway.app/pi" // Correct backend URL without port
 );
 const API = axios.create({
   baseURL: base_url,
