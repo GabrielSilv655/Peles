289ea7524324c90c04c4f584d6a3954964d943e9
gabriel <githubgabriel21@gmail.com>
2025-07-15 22:38:09 -0300

    alterando2

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'sisa',
+  process.env.DB_NAME || 'railway',
   process.env.DB_USER || 'root',
   process.env.DB_PASSWORD || '',
   {
