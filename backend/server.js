a0161a01503647afd1f480a0809b0975801b6478
gabriel <githubgabriel21@gmail.com>
2025-10-22 16:51:37 -0300

    pequeno testes

--- backend/server.js
+++ backend/server.js
@@ -159,7 +159,8 @@ if (process.env.NODE_ENV === 'production') {
     origin: ['https://localhost:3000', 'https://127.0.0.1:3000', 
              'http://localhost:3000', 'http://127.0.0.1:3000',
              'https://localhost:8080', 'https://127.0.0.1:8080',
-             'http://localhost:8080', 'http://127.0.0.1:8080'],
+             'http://localhost:8080', 'http://127.0.0.1:8080',
+             'https://sisa-project.up.railway.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
